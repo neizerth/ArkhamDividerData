893429411a8d19960fc33eeded8e6d5bbe175c0a
Vladimir Yazykov <neizerth@gmail.com>
2024-09-08 23:59:17 +0300

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export * from './cache';
+export * from './cache/index';
 export * from './cache/campaigns/getCampaignsCache';
 export * from './cache/cycles/getCyclesCache';
 export * from './cache/encounterSets/getEncounterSetsCache';
