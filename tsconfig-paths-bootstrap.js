12f1551450c05afc106c38b96a4321eb555f4a01
Vladimir Yazykov <neizerth@gmail.com>
2024-09-09 06:56:58 +0300

    upd

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -1,7 +1,8 @@
 const tsConfig = require("./tsconfig.json");
 const tsConfigPaths = require("tsconfig-paths");
+const path = require("path");
 
-const baseUrl = "./build"; // Either absolute or relative path. If relative it's resolved to current working directory.
+const baseUrl = path.join(__dirname, '/build'); // Either absolute or relative path. If relative it's resolved to current working directory.
 
 tsConfigPaths.register({
   baseUrl,
