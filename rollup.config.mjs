b5ce9930d218317ffb838682fa26a17f9070ae2e
Vladimir Yazykov <neizerth@gmail.com>
2024-09-09 12:34:18 +0300

    moved to rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,16 @@
+import typescript from '@rollup/plugin-typescript';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'build',
+    format: 'cjs'
+  },
+  plugins: [
+    json(),
+    typescript(),
+    typescriptPaths(),
+  ]
+};
