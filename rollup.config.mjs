491562ec7c3351319154c07b76bb900fb3cf918e
Vladimir Yazykov <neizerth@gmail.com>
2024-09-09 12:38:58 +0300

    test

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,7 +10,10 @@ export default {
   },
   plugins: [
     json(),
-    typescript(),
-    typescriptPaths(),
+    typescript({
+    }),
+    typescriptPaths({
+      nonRelative: true,
+    }),
   ]
 };
