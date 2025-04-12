da8a859453a8f27e0e6557aed1bd921f8be775d4
Vladimir Yazykov <neizerth@gmail.com>
2025-04-12 14:05:47 +0300

    fix expedition

--- src/components/data/getEncounterSets.ts
+++ src/components/data/getEncounterSets.ts
@@ -1,54 +1,48 @@
-import * as ArkhamDB from '@/components/arkhamDB';
-import * as ArkhamCards from '@/components/arkhamCards' 
-import { groupBy, prop, propEq, toPairs } from 'ramda';
-import { whereSynonyms } from '@/util/criteria';
-import { showError } from '@/util/console';
+import * as ArkhamDB from "@/components/arkhamDB";
+import * as ArkhamCards from "@/components/arkhamCards";
+import { groupBy, prop, propEq, toPairs } from "ramda";
+import { whereSynonyms } from "@/util/criteria";
+import { showError } from "@/util/console";
+
+const SPECIAL_GROUP_NAMES = ["Epic Multiplayer", "Single Group"];
 
 export const getEncounterSets = async () => {
-  console.log('loading Arkham Cards encounter sets...');
+  console.log("loading Arkham Cards encounter sets...");
   const arkhamCardsEncounters = await ArkhamCards.getEncounterSets();
 
-  console.log('loading ArkhamDB encounter sets...');
+  console.log("loading ArkhamDB encounter sets...");
   const arkhamDBEncounters = await ArkhamDB.getEncounterSets();
 
-  const specialGroups = toPairs(
-    groupBy(
-      prop('name'), 
-      arkhamDBEncounters
-    )
-  )
-  .map(([name, group = []]) => {
-    return {
-      name,
-      group
-    }
-  })
-  .filter(({ group }) => {
-    return group.length > 1;
-  })
-  
+  const specialGroups = toPairs(groupBy(prop("name"), arkhamDBEncounters))
+    .map(([name, group = []]) => {
+      return {
+        name,
+        group,
+      };
+    })
+    .filter(({ group }) => {
+      return group.length > 1;
+    });
 
-  const specialNames = specialGroups.map(prop('name'));
+  // const specialNames = specialGroups.map(prop('name'));
 
-  console.log(`found special groups: ${specialNames.join(', ')}...`);
-  
+  // console.log(`found special groups: ${specialNames.join(", ")}...`);
+
   const prepareText = (text: string) => text.trim().toLowerCase();
 
-  const matches = arkhamDBEncounters.map(encounter => {
-    const specialGroup = specialGroups.find(
-      propEq(encounter.name, 'name')
-    );
+  const matches = arkhamDBEncounters.map((encounter) => {
+    const specialGroup = specialGroups.find(propEq(encounter.name, "name"));
     if (specialGroup) {
       const synonyms = [
         ...encounter.synonyms,
         ...specialGroup.group
-          .map(prop('code'))
-          .filter(code => code !== encounter.code)
+          .map(prop("code"))
+          .filter((code) => code !== encounter.code),
       ];
-      
+
       return {
         ...encounter,
-        synonyms
+        synonyms,
       };
     }
 
@@ -61,31 +55,27 @@ export const getEncounterSets = async () => {
 
       return encounter;
     }
-    
+
     if (encounter.code === arkhamCardsEncounter.code) {
       return encounter;
     }
 
-    console.log(`found encounter code difference: ${encounter.code}/${arkhamCardsEncounter.code}`)
+    console.log(
+      `found encounter code difference: ${encounter.code}/${arkhamCardsEncounter.code}`
+    );
 
     return {
       ...encounter,
-      synonyms: [
-        ...encounter.synonyms,
-        arkhamCardsEncounter.code
-      ]
-    }
+      synonyms: [...encounter.synonyms, arkhamCardsEncounter.code],
+    };
   });
-  
+
   const restArkhamCards = arkhamCardsEncounters.filter(({ code }) => {
-    if (specialNames.includes(code)) {
+    if (SPECIAL_GROUP_NAMES.includes(code)) {
       return false;
     }
     return !arkhamDBEncounters.some(whereSynonyms(code));
   });
 
-  return [
-    ...matches,
-    ...restArkhamCards
-  ]
-}
+  return [...matches, ...restArkhamCards];
+};
