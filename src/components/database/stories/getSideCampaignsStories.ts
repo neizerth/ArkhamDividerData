7f3678492822533b5e21e832a59508168aea45f3
Vladimir Yazykov <neizerth@gmail.com>
2025-05-08 06:47:33 +0300

    fix: fof campaign type / standalone icons

--- src/components/database/stories/getSideCampaignsStories.ts
+++ src/components/database/stories/getSideCampaignsStories.ts
@@ -1,8 +1,8 @@
 import { createIconDB } from "@/components/arkhamCards/icons/IconDB";
 import { IDatabase } from "@/types/database";
-import * as Cache from '@/util/cache';
+import * as Cache from "@/util/cache";
 import { showError, showWarning } from "@/util/console";
-import packsData from '@/data/arkhamCards/packs'
+import packsData from "@/data/arkhamCards/packs";
 
 import { isNotNil, prop, propEq } from "ramda";
 import type { SingleValue } from "@/types/common";
@@ -23,97 +23,97 @@ export const getSideCampaignStories = (): IDatabase.Story[] => {
   const packInvestigators = Cache.getPackInvestigators();
 
   const withoutSizeSupport = packsData
-    .filter(propEq(false, 'is_size_supported'))
-    .map(prop('pack_code'));
+    .filter(propEq(false, "is_size_supported"))
+    .map(prop("pack_code"));
 
   const iconDB = createIconDB(IconDBType.STORY);
-  
+
   const getStoryScenario = createStoryScenarioHandler({
     iconDB,
     scenarioEncounters: scenarioEncounterSets,
-    encounterSets
-  })
+    encounterSets,
+  });
 
   const sideCampaignIds = sideScenarios
     .filter(
-      ({ cycle_code, scenario_id }) => !scenario_id && cycle_code === SIDE_STORY_CODE
+      ({ cycle_code, scenario_id }) =>
+        !scenario_id && cycle_code === SIDE_STORY_CODE
     )
-    .map(prop('campaign_id'))
-    .filter(isNotNil)
+    .map(prop("campaign_id"))
+    .filter(isNotNil);
 
   return fullCampaigns
     .filter(({ campaign }) => sideCampaignIds.includes(campaign.id))
     .map(({ campaign, scenarios }) => {
-      
-      const link = sideScenarios.find(({ campaign_id }) => campaign_id === campaign.id);
-      
+      const link = sideScenarios.find(
+        ({ campaign_id }) => campaign_id === campaign.id
+      );
+
       if (!link) {
         showError(`link not found: ${campaign.id}`);
         return;
       }
 
-      const pack = packs.find(propEq(link.pack_code, 'code'));
+      const pack = packs.find(propEq(link.pack_code, "code"));
 
       if (!pack) {
         showWarning(`pack not found: ${link.pack_code}`);
       }
-      
+
       const scenarioEncounters = scenarioEncounterSets.filter(
-        propEq(campaign.id, 'campaign_id')
+        propEq(campaign.id, "campaign_id")
       );
 
-      const requiredEncounters = scenarioEncounters.filter(
-        propEq(false, 'is_extra')
-      )
-      .map(prop('encounter_set_code'));
+      const requiredEncounters = scenarioEncounters
+        .filter(propEq(false, "is_extra"))
+        .map(prop("encounter_set_code"));
 
-      const extraEncounters = scenarioEncounters.filter(
-        propEq(true, 'is_extra')
-      )
-      .map(prop('encounter_set_code'));
+      const extraEncounters = scenarioEncounters
+        .filter(propEq(true, "is_extra"))
+        .map(prop("encounter_set_code"));
 
       const name = pack?.name || campaign.name;
-      
+
       const {
         is_official,
         is_canonical,
         code = campaign.id,
-      } = pack || {} as SingleValue<typeof packs>;
+      } = pack || ({} as SingleValue<typeof packs>);
 
-      const isSizeSupported = is_official && 
-        is_canonical && 
-        !withoutSizeSupport.includes(code);
+      const isSizeSupported =
+        is_official && is_canonical && !withoutSizeSupport.includes(code);
 
       const icon = iconDB.getIcon(code);
-      const type = IDatabase.StoryType.SIDE_CAMPAIGN;
+      // const type = IDatabase.StoryType.SIDE_CAMPAIGN;
+      const type = IDatabase.StoryType.SIDE;
 
-      const storyScenarios = scenarios
-        .map(
-          scenario => getStoryScenario({
-            campaignId: campaign.id,
-            scenario
-          })
-        );
+      const storyScenarios = scenarios.map((scenario) =>
+        getStoryScenario({
+          campaignId: campaign.id,
+          scenario,
+        })
+      );
 
       const storyScenarioGroups = groupStoryScenarios({
         iconDB,
-        scenarios: storyScenarios
-      })
+        scenarios: storyScenarios,
+      });
 
       const storyScenarioEncounters = getStoryScenarioEncounters({
         encounterSets,
-        scenarios: storyScenarios
+        scenarios: storyScenarios,
       });
 
-      const investigators = pack ? 
-        packInvestigators.filter(
-          propEq(pack.code, 'pack_code')
-        ) : [];
+      const investigators = pack
+        ? packInvestigators.filter(propEq(pack.code, "pack_code"))
+        : [];
 
-      const customContent = campaign.custom && getStoryCustomContent({
-        code,
-        content: campaign.custom
-      });
+      const customContent =
+        campaign.custom &&
+        getStoryCustomContent({
+          code,
+          content: campaign.custom,
+        });
 
       return {
         name,
@@ -131,8 +131,8 @@ export const getSideCampaignStories = (): IDatabase.Story[] => {
         is_official,
         scenario_encounter_sets: storyScenarioEncounters,
         encounter_sets: requiredEncounters,
-        extra_encounter_sets: extraEncounters
-      }
+        extra_encounter_sets: extraEncounters,
+      };
     })
-    .filter(isNotNil)
-}
+    .filter(isNotNil);
+};
