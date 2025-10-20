8be930242a38c2e1ed8bc1abf0e42ac90d5b2ca3
Vladimir Yazykov <neizerth@gmail.com>
2025-10-20 16:19:18 +0300

    fix: roulette

--- src/data/icons/special/common.ts
+++ src/data/icons/special/common.ts
@@ -1,167 +1,172 @@
 export default [
-  {
-    'icon': 'en',
-    'width': 1024,
-    'height': 1024
-  },
-  {
-    'icon': 't_bank',
-    'width': 551,
-    'height': 551
-  },
-  {
-    'icon': 'ffg',
-    'width': 1024,
-    'height': 1024
-  },
-  {
-    'icon': 'etsy',
-    'width': 48,
-    'height': 24
-  },
-  {
-    'icon': 'bgg',
-    'width': 16,
-    'height': 24
-  },
-  {
-    'icon': 'meeple',
-    'width': 800,
-    'height': 800
-  },
-  {
-    'icon': 'multiclass_arkhamesque',
-    'width': 27,
-    'height': 24
-  },
-  {
-    'icon': 'mystic_alt',
-    'width': 10,
-    'height': 9.08
-  },
-  {
-    'icon': 'patreon',
-    'width': 1080,
-    'height': 1080
-  },
-  {
-    'icon': 's_frame_background_null',
-    'width': 1030,
-    'height': 1024
-  },
-  {
-    'icon': 'boosty',
-    'width': 157,
-    'height': 189
-  },
-  {
-    'icon': 'unique',
-    'width': 405,
-    'height': 301
-  },
-  {
-    'icon': 'plus',
-    'width': 145,
-    'height': 145
-  },
-  {
-    'icon': 'minus',
-    'width': 118,
-    'height': 29
-  },
-  {
-    'icon': 'plus-outline',
-    'width': 1160,
-    'height': 1160
-  },
-  {
-    'icon': 'minus-outline',
-    'width': 1160,
-    'height': 410
-  },
-  {
-    'icon': 'plus-fill',
-    'width': 1160,
-    'height': 1160
-  },
-  {
-    'icon': 'minus-fill',
-    'width': 1160,
-    'height': 410
-  },
-  {
-    'icon': 'num0-outline_alt',
-    'width': 1024,
-    'height': 1024
-  },
-  {
-    'icon': 'change-investigator',
-    'width': 535,
-    'height': 555
-  },
-  {
-    'icon': 'token-modification',
-    'width': 270,
-    'height': 270
-  },
-  {
-    'icon': 'check-fill',
-    'width': 700,
-    'height': 700
-  },
-  {
-    'icon': 'check-outline_bold',
-    'width': 700,
-    'height': 700
-  },
-  {
-    'icon': 'check-outline',
-    'width': 700,
-    'height': 895
-  },
-  {
-    'icon': 'blocked-medium',
-    'width': 512,
-    'height': 512
-  },
-   {
-    'icon': 'blocked-thin',
-    'width': 512,
-    'height': 512
-  },
-  {
-    'icon': 'effect',
-    'width': 512,
-    'height': 512
-  },
-  {
-    'icon': 'blocked-outline',
-    'width': 512,
-    'height': 512
-  },
-  {
-    'icon': 'token_auto_fail_highlight_inverse',
-    'width': 868,
-    'height': 868
-  },
-  {
-    'icon': 'exhaust',
-    'width': 863,
-    'height': 1022
-  },
-  {
-    'icon': 'claws',
-    'width': 295,
-    'height': 298
-  },
-  {
-    'icon': 'token_moon_fill',
-    'width': 1024,
-    'height': 1024
-  },
-  {
-    'icon': 'token_moon_outline',
-    'width': 1024,
-    'height': 1024
-  }
-]
+	{
+		icon: "en",
+		width: 1024,
+		height: 1024,
+	},
+	{
+		icon: "t_bank",
+		width: 551,
+		height: 551,
+	},
+	{
+		icon: "ffg",
+		width: 1024,
+		height: 1024,
+	},
+	{
+		icon: "etsy",
+		width: 48,
+		height: 24,
+	},
+	{
+		icon: "bgg",
+		width: 16,
+		height: 24,
+	},
+	{
+		icon: "meeple",
+		width: 800,
+		height: 800,
+	},
+	{
+		icon: "multiclass_arkhamesque",
+		width: 27,
+		height: 24,
+	},
+	{
+		icon: "mystic_alt",
+		width: 10,
+		height: 9.08,
+	},
+	{
+		icon: "patreon",
+		width: 1080,
+		height: 1080,
+	},
+	{
+		icon: "s_frame_background_null",
+		width: 1030,
+		height: 1024,
+	},
+	{
+		icon: "boosty",
+		width: 157,
+		height: 189,
+	},
+	{
+		icon: "unique",
+		width: 405,
+		height: 301,
+	},
+	{
+		icon: "plus",
+		width: 145,
+		height: 145,
+	},
+	{
+		icon: "minus",
+		width: 118,
+		height: 29,
+	},
+	{
+		icon: "plus-outline",
+		width: 1160,
+		height: 1160,
+	},
+	{
+		icon: "minus-outline",
+		width: 1160,
+		height: 410,
+	},
+	{
+		icon: "plus-fill",
+		width: 1160,
+		height: 1160,
+	},
+	{
+		icon: "minus-fill",
+		width: 1160,
+		height: 410,
+	},
+	{
+		icon: "num0-outline_alt",
+		width: 1024,
+		height: 1024,
+	},
+	{
+		icon: "change-investigator",
+		width: 535,
+		height: 555,
+	},
+	{
+		icon: "token-modification",
+		width: 270,
+		height: 270,
+	},
+	{
+		icon: "check-fill",
+		width: 700,
+		height: 700,
+	},
+	{
+		icon: "check-outline_bold",
+		width: 700,
+		height: 700,
+	},
+	{
+		icon: "check-outline",
+		width: 700,
+		height: 895,
+	},
+	{
+		icon: "blocked-medium",
+		width: 512,
+		height: 512,
+	},
+	{
+		icon: "blocked-thin",
+		width: 512,
+		height: 512,
+	},
+	{
+		icon: "effect",
+		width: 512,
+		height: 512,
+	},
+	{
+		icon: "blocked-outline",
+		width: 512,
+		height: 512,
+	},
+	{
+		icon: "token_auto_fail_highlight_inverse",
+		width: 868,
+		height: 868,
+	},
+	{
+		icon: "exhaust",
+		width: 863,
+		height: 1022,
+	},
+	{
+		icon: "claws",
+		width: 295,
+		height: 298,
+	},
+	{
+		icon: "token_moon_fill",
+		width: 1024,
+		height: 1024,
+	},
+	{
+		icon: "token_moon_outline",
+		width: 1024,
+		height: 1024,
+	},
+	{
+		icon: "roulette",
+		width: 87,
+		height: 88,
+	},
+];
