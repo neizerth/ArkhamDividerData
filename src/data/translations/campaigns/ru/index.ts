f996d711a7b248077e445c0fb33d78a994740138
Vladimir Yazykov <neizerth@gmail.com>
2026-02-16 07:52:16 +0100

    feat: zdm / zrttic ru

--- src/data/translations/campaigns/ru/index.ts
+++ src/data/translations/campaigns/ru/index.ts
@@ -1,9 +1,9 @@
-export { default as zhod } from './zhod.json'; 
-export { default as zau } from './zau.json';
-export { default as ages_unwound } from './zau.json';
-export { default as winter_winds } from './winter_winds.json';
-
-export { default as zbh } from './zbh.json';
-export { default as zcc } from './zcc.json';
-export { default as zez } from './zez.json';
-export { default as tdc } from './tdc.json';
+export { default as winter_winds } from "./winter_winds.json";
+export { default as ages_unwound, default as zau } from "./zau.json";
+export { default as zbh } from "./zbh.json";
+export { default as zcc } from "./zcc.json";
+export { default as zdm } from "./zdm.json";
+export { default as zdms } from "./zdms.json";
+export { default as zez } from "./zez.json";
+export { default as zhod } from "./zhod.json";
+export { default as zrttic } from "./zrttic.json";
