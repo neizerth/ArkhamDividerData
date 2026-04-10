23cd2fcc95c9f4a7355dc6eeec15d08862afdcce
Vladimir Yazykov <neizerth@gmail.com>
2026-04-10 12:20:46 +0300

    feat: zaw

--- src/data/translations/campaigns/ru/index.ts
+++ src/data/translations/campaigns/ru/index.ts
@@ -1,5 +1,6 @@
 export { default as winter_winds } from "./winter_winds.json";
 export { default as ages_unwound, default as zau } from "./zau.json";
+export { default as zaw } from "./zaw.json";
 export { default as zbh } from "./zbh.json";
 export { default as zcc } from "./zcc.json";
 export { default as zdm } from "./zdm.json";
@@ -7,3 +8,4 @@ export { default as zdms } from "./zdms.json";
 export { default as zez } from "./zez.json";
 export { default as zhod } from "./zhod.json";
 export { default as zrttic } from "./zrttic.json";
+
